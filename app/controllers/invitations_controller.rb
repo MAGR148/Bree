eaad8560d4c90763b1a24c573dc0a849e8625a14
Raul Palacios <palacios.raul02@gmail.com>
2021-06-13 21:04:51 -0500

    Change redirection to root_path when send a invitation

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,68 +1,45 @@
 class InvitationsController < ApplicationController
   before_action :set_invitation, only: %i[ show edit update destroy ]
 
-  # GET /invitations or /invitations.json
   def index
     @invitations = Invitation.all
   end
 
-  # GET /invitations/1 or /invitations/1.json
-  def show
-  end
+  def show; end
 
-  # GET /invitations/new
   def new
     @invitation = Invitation.new
   end
 
-  # GET /invitations/1/edit
-  def edit
-  end
+  def edit; end
 
-  # POST /invitations or /invitations.json
   def create
     @invitation = Invitation.new(invitation_params)
-
-    respond_to do |format|
-      if @invitation.save
-        format.html { redirect_to @invitation, notice: "Invitation was successfully created." }
-        format.json { render :show, status: :created, location: @invitation }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @invitation.errors, status: :unprocessable_entity }
-      end
+    if @invitation.save
+      redirect_to root_path, notice: 'Invitation was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /invitations/1 or /invitations/1.json
   def update
-    respond_to do |format|
-      if @invitation.update(invitation_params)
-        format.html { redirect_to @invitation, notice: "Invitation was successfully updated." }
-        format.json { render :show, status: :ok, location: @invitation }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @invitation.errors, status: :unprocessable_entity }
-      end
+    if @invitation.update(invitation_params)
+      redirect_to @invitation, notice: 'Invitation was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
-  # DELETE /invitations/1 or /invitations/1.json
   def destroy
     @invitation.destroy
-    respond_to do |format|
-      format.html { redirect_to invitations_url, notice: "Invitation was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to invitations_url, notice: 'Invitation was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_invitation
       @invitation = Invitation.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def invitation_params
       params.require(:invitation).permit(:name, :email, :city, :nutrition_professional)
     end
