4c191e0d95950673934818feb504a55cad7ccc89
Raul Palacios <palacios.raul02@gmail.com>
2021-07-08 23:20:55 -0500

    Remove unnecesary macros action

--- app/controllers/admin/macros_controller.rb
+++ app/controllers/admin/macros_controller.rb
@@ -1,10 +1,5 @@
 module Admin
   class MacrosController < BaseController
-    def new
-      @calories = 1100
-      @weight = 90
-    end
-    
     def edit
       @calories = 1100
       @weight = 90
