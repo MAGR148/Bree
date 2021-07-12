668775cdde19d4ec7c6589e1ae39d57267f6a2c5
Raul Palacios <palacios.raul02@gmail.com>
2021-07-12 00:18:50 -0500

    add operations for group_portion table

---
+++ app/javascript/controllers/group_portion_controller.js
@@ -0,0 +1,39 @@
+import { Controller } from 'stimulus';
+
+export default class extends Controller {
+  static values = {
+    carbohydrates: Number,
+    protein: Number,
+    lipids: Number,
+    energy: Number
+  }
+
+  static targets = [
+    'carbohydrates',
+    'protein',
+    'lipids',
+    'energy',
+    'quantity',
+  ]
+
+  connect() {
+    this.setGroupValues();
+    this.quantityTarget.addEventListener("change", (e) => this.multiply_groups(parseInt(e.target.value)))
+  }
+
+  setGroupValues(values = {carbohydrates: 0, protein: 0, lipids: 0, energy: 0}) {
+    this.carbohydratesTarget.innerHTML = `${values.carbohydrates} grs`
+    this.proteinTarget.innerHTML = `${values.protein} grs`
+    this.lipidsTarget.innerHTML = `${values.lipids} grs`
+    this.energyTarget.innerHTML = `${values.energy} kcal`
+  }
+
+  multiply_groups(multiple) {
+    this.setGroupValues({
+      carbohydrates: parseInt(this.carbohydratesValue) * multiple,
+      protein: parseInt(this.proteinValue) * multiple,
+      lipids: parseInt(this.lipidsValue) * multiple,
+      energy: parseInt(this.energyValue) * multiple,
+    })
+  }
+}
