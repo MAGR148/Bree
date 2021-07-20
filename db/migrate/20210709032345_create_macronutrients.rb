dcf9d966d3471fc7a0e0197b8fb4e9ab9cdc7b94
Raul Palacios <palacios.raul02@gmail.com>
2021-07-20 11:51:58 -0500

    Add grams by default

--- db/migrate/20210709032345_create_macronutrients.rb
+++ db/migrate/20210709032345_create_macronutrients.rb
@@ -2,9 +2,9 @@ class CreateMacronutrients < ActiveRecord::Migration[6.1]
   def change
     create_table :macronutrients do |t|
       t.references :plan, null: false, foreign_key: true
-      t.jsonb :carbohydrates, null: false, default: { percentage: 50 }
-      t.jsonb :protein, null: false, default: { percentage: 25 }
-      t.jsonb :lipids, null: false, default: { percentage: 25 }
+      t.jsonb :carbohydrates, null: false, default: { percentage: 50, grams: 287.50 }
+      t.jsonb :protein, null: false, default: { percentage: 25, grams: 143.75 }
+      t.jsonb :lipids, null: false, default: { percentage: 25, grams: 63.89 }
 
       t.timestamps
     end
