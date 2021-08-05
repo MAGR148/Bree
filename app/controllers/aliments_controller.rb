b2717fedf70c6fa91cc6f0a54686dfb1b6ee1bb2
Raul Palacios <palacios.raul02@gmail.com>
2021-08-04 23:43:07 -0500

    Add form for edit aliments

--- app/controllers/aliments_controller.rb
+++ app/controllers/aliments_controller.rb
@@ -1,5 +1,23 @@
 class AlimentsController < ApplicationController
-  def index
-    # @aliments = Aliment.all
+  def index; end
+
+  def edit
+    @aliment = Aliment.find(params[:id])
+  end
+
+  def update
+    @aliment = Aliment.find(params[:id])
+
+    if @aliment.update(aliment_params)
+      redirect_to aliments_path, notice: 'Aliment updated successfully'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def aliment_params
+    params.require(:aliment).permit!
   end
 end
