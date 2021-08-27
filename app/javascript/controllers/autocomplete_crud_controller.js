28e4bb214c408b162613581fdc4ec6da8c38ffac
Raul Palacios <palacios.raul02@gmail.com>
2021-08-26 22:40:09 -0500

    Show ingredients (WP)

--- app/javascript/controllers/autocomplete_crud_controller.js
+++ app/javascript/controllers/autocomplete_crud_controller.js
@@ -11,7 +11,7 @@ export default class extends Controller {
     connect() {
       const selectedValues = JSON.parse(`[${this.crudInputTarget.value}]`);
       
-      selectedValues.map((ingredient) => this.renderStudies(ingredient.value, ingredient.group, ingredient.unit, true));
+      selectedValues.map((ingredient) => this.renderStudies(ingredient.value, ingredient.group, ingredient.unit));
       this.resultsTarget.addEventListener('click', this.onResultsClick);
       this.inputTarget.addEventListener('keydown', this.onKeyDown);
     }
@@ -43,32 +43,36 @@ export default class extends Controller {
       }
     }
 
-    renderStudies = (value, group = '', unit = '', saved = false) => {
-      const customID = Date.now();
-      const tr = document.createElement('tr');
-      tr.setAttribute('id', customID);
-      tr.classList.add('ml-1', 'mt-3', 'is-flex', 'is-align-items-center');
-      tr.innerHTML = `
-        <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-          ${value}
-        </td>
-        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-          ${group}
-        </td>
-        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-          ${unit}
-        </td>
-        <td class="pl-4 pr-0 py-4 whitespace-nowrap text-right text-sm font-medium">
-          <a
-            class="text-red-600 hover:text-red-900"
-            data-action="${saved || 'click->autocomplete-crud#deleteSelectedElement'}" value="${value}">
-              <svg data-custom-index="${customID}" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-              </svg>
-          </a>
-        </td>`;
-
-      this.selectedValuesTarget.appendChild(tr);
+    renderStudies = (value, group = '', unit = '') => {
+      setTimeout(() => { 
+        const customID = Date.now();
+        const tr = document.createElement('tr');
+        tr.setAttribute('id', customID);
+        tr.classList.add('ml-1', 'mt-3', 'is-flex', 'is-align-items-center');
+        tr.innerHTML = `
+          <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+            ${value}
+          </td>
+          <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+            ${group}
+          </td>
+          <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+            ${unit}
+          </td>
+          <td class="pl-4 pr-0 py-4 whitespace-nowrap text-right text-sm font-medium">
+            <a
+              class="text-red-600 hover:text-red-900"
+              data-action="click->autocomplete-crud#deleteSelectedElement" 
+              value="${value}"
+            >
+                <svg data-unique-index="${customID}" data-unique-value="${value}" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                </svg>
+            </a>
+          </td>`;
+  
+        this.selectedValuesTarget.appendChild(tr);
+      }, 500)
     }
     
     saveArray = (value, group, unit) => {
@@ -80,15 +84,20 @@ export default class extends Controller {
       this.crudInputTarget.value = studiesArray.join();
     }
 
-    deleteSelectedElement = ({ currentTarget: { value } }) => {
-      const studies = this.crudInputTarget.value || '';
-
-      const studiesArray = studies.split(',').filter((e) => e !== '' && e !== value);
-
-      document.getElementById(value).remove();
-
-      this.crudInputTarget.value = studiesArray.join();
-
+    deleteSelectedElement = (e) => {
+      const customIndex = e.target.dataset.uniqueIndex
+      const el = document.getElementById(customIndex)
+      
+      if(el){
+        el.remove()
+        const uniqueValue = e.target.dataset.uniqueValue
+        const selectedValues = JSON.parse(`[${this.crudInputTarget.value}]`);
+        
+        const values = selectedValues.filter((e) => e.value !== '' && e.value !== uniqueValue);
+        const stringValues = JSON.stringify(values);
+        
+        this.crudInputTarget.value = stringValues.substring(1, stringValues.length - 1);
+      }
     }
 
     extractTextValue = (el) => (el.hasAttribute('data-autocomplete-label')
