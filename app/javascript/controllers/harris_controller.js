3567d53cdcbc3e9d5bc22ec2e9cb68a1e1022f23
Raul Palacios <palacios.raul02@gmail.com>
2021-08-28 21:44:04 -0500

    Add physucal activity to harris and mifflin operations

--- app/javascript/controllers/harris_controller.js
+++ app/javascript/controllers/harris_controller.js
@@ -2,13 +2,14 @@ import { Controller } from 'stimulus';
 
 export default class extends Controller {
 
-  static targets = ['ger', 'eta'];
+  static targets = ['ger', 'eta', 'result'];
 
   static values = { 
     gender: String, 
     weight: Number,
     height: Number,
     age: Number,
+    physicalActivity: Number
   };
 
   connect(){
@@ -18,10 +19,15 @@ export default class extends Controller {
   showResults(){
     this.gerTarget.innerHTML = this.result();
     this.etaTarget.innerHTML = this.resultPercentage();
+    this.resultTarget.innerHTML = this.harrisResult();
   }
 
   result = _ => this.isMale ? this.man_calculation : this.woman_calculation
   resultPercentage = _ => (this.result() / 100 * 10).toFixed(2)
+  harrisResult = _ => {
+    const result = this.result() * parseFloat(this.physicalActivityValue) + this.resultPercentage()
+    return parseFloat(result).toFixed(2)
+  }
 
   get isMale(){
     return this.genderValue === 'M' ? true : false;
--- app/javascript/controllers/mifflin_controller.js
+++ app/javascript/controllers/mifflin_controller.js
@@ -2,13 +2,14 @@ import { Controller } from 'stimulus';
 
 export default class extends Controller {
 
-  static targets = ['ger'];
+  static targets = ['ger', 'result'];
 
   static values = { 
     gender: String, 
     weight: Number,
     height: Number,
     age: Number,
+    physicalActivity: Number
   };
 
   connect(){
@@ -17,9 +18,14 @@ export default class extends Controller {
 
   showResults(){
     this.gerTarget.innerHTML = this.result();
+    this.resultTarget.innerHTML = this.mifflinResult();
   }
 
   result = _ => this.isMale ? this.man_calculation : this.woman_calculation
+  mifflinResult = _ => {
+    const result = this.result() * this.physicalActivityValue
+    return parseFloat(result).toFixed(2)
+  }
 
   get isMale(){
     return this.genderValue === 'M' ? true : false;
