00a3e1645a8ba96547faa4978621e287eba9dee9
Raul Palacios <palacios.raul02@gmail.com>
2021-08-28 20:33:09 -0500

    Remove unnecesary calories selector and persist in input calories saved

--- app/javascript/controllers/calories_register_controller.js
+++ app/javascript/controllers/calories_register_controller.js
@@ -3,6 +3,12 @@ import { Controller } from 'stimulus';
 export default class extends Controller {
   static targets = [ 'input' ]
 
+  static values = { calories: Number }
+
+  initialize(){
+    this.inputTarget.value = this.caloriesValue
+  }
+
   writeCalories(){
     window.dispatchEvent(new CustomEvent('selectedCalories', {
       detail: this.inputTarget.value
