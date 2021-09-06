32f20558ce5420acfaeb2c99fb57f86098b8d0fc
Raul Palacios <palacios.raul02@gmail.com>
2021-09-05 23:22:05 -0500

    Add modal for close plan assignment

---
+++ app/javascript/controllers/modal_controller.js
@@ -0,0 +1,18 @@
+import { Controller } from 'stimulus'
+
+export default class extends Controller {
+  
+  static targets = [ 'modal' ]
+
+  connect(){
+    this.hide();
+  }
+
+  show(){
+    this.modalTarget.style.display = 'block';
+  }
+
+  hide(){
+    this.modalTarget.style.display = 'none';
+  }
+}
