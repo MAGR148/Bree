f64f91a9f5c2a52b35c63a22b24f19b5b2feab70
Raul Palacios <palacios.raul02@gmail.com>
2021-10-18 01:50:58 -0500

    Add limit to recipes in times results

--- app/controllers/admin/recipe_times_controller.rb
+++ app/controllers/admin/recipe_times_controller.rb
@@ -3,7 +3,7 @@
 module Admin
   class RecipeTimesController < BaseController
     def edit
-      @recipes = Recipe.where(food_time_priority: I18n.t(params[:recipe_time]))
+      @recipes = Recipe.where(food_time_priority: I18n.t(params[:recipe_time])).limit(20)
       @patient = Patient.find(params[:patient_id])
       @time = params[:recipe_time]
     end
