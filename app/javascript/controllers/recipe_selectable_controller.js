9e322b6a7040d7d88c0cc908a0e03c687f5ee459
Raul Palacios <palacios.raul02@gmail.com>
2021-10-17 20:57:50 -0500

    Add new selectable functionality to recipes

---
+++ app/javascript/controllers/recipe_selectable_controller.js
@@ -0,0 +1,36 @@
+import { Controller } from 'stimulus'
+
+export default class extends Controller {
+  static targets = [ 'recipe' ]
+
+  select(e){
+    this.defineOrGetRecipe(e.currentTarget);
+  }
+
+  defineOrGetRecipe(recipe){
+    if (localStorage.getItem('recipeId') === null) {
+      this.storeRecipe();
+      this.paintSelected(recipe);
+    } else {
+      this.unpaintSelected(recipe);
+      this.storeRecipe();
+      this.paintSelected(recipe);
+    }
+  }
+
+  storeRecipe(recipe){
+    localStorage.setItem('recipeId', `${recipe}`);
+  }
+
+  paintSelected(recipe){
+    recipe.classList.add('ring-2');
+    recipe.classList.add('ring-offset-2');
+    recipe.classList.add('ring-indigo-500');
+  }
+
+  unpaintSelected(recipe){
+    recipe.classList.remove('ring-2');
+    recipe.classList.remove('ring-offset-2');
+    recipe.classList.remove('ring-indigo-500');
+  }
+}
