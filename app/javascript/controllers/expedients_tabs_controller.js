06945c83f4100bf68ccd64001b27c884eef2c2ea
Miguel Gomez <miggr14@gmail.com>
2021-10-26 15:16:21 -0500

    Add stimulus controller for expident's tabs

---
+++ app/javascript/controllers/expedients_tabs_controller.js
@@ -0,0 +1,18 @@
+import { Controller } from 'stimulus'
+
+export default class extends Controller {
+  
+  static targets = [ 'option' ];
+
+  selectTab(e){
+    this.optionTargets.forEach((el, i) => {
+      el.classList.replace('bg-indigo-50', 'text-gray-500')
+      el.classList.replace('text-indigo-700', 'hover:text-gray-700') 
+      
+      if(el === e.currentTarget) {
+        el.classList.replace('text-gray-500', 'bg-indigo-50')
+        el.classList.replace('hover:text-gray-700', 'text-indigo-700') 
+      }
+    })
+  }
+}
