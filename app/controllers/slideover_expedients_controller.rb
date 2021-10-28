8a316dcb066c49652cd150c1ed163f0ed17bae72
Ángela González <90643393+AngelaMCGonzalez@users.noreply.github.com>
2021-10-27 23:50:32 -0500

    Fix: Changes in the slideover tabs (#18)
    * Prueba
    
    * UI slideover expediente
    
    * Pattern changes
    
    * Interfaces changes

---
+++ app/controllers/slideover_expedients_controller.rb
@@ -0,0 +1,14 @@
+class SlideoverExpedientsController < ApplicationController
+  def general
+  end
+  def habits
+  end
+  def history
+  end
+  def physical_activity
+  end
+  def notes
+  end
+  def anthropometric
+  end
+end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,7 +10,7 @@ import "@hotwired/turbo-rails"
 
 // import * as ActiveStorage from "@rails/activestorage"
 // import "channels"
-import '../stylesheets/application.scss'
+import '../stylesheets/application.scss' //import tailwindcss
 import "controllers"
 Rails.start()
 
