de190b2ce12e9f4f267a3674120d0a435ab5b7fb
Miguel Gomez <miggr14@gmail.com>
2021-11-09 19:12:19 -0600

    Avoiding turbo frame overwriting

--- app/javascript/controllers/operation_tabs_controller.js
+++ app/javascript/controllers/operation_tabs_controller.js
@@ -6,10 +6,23 @@ export default class extends Controller {
     id: String,
   }
 
-  changeTab(e) {
+   //Function used for support calculation tabs
+   changeTab(e) {
     e.preventDefault();
 
     window.dispatchEvent(new CustomEvent('refreshOperationTurboFrame', {
+      detail: {
+        url: this.urlValue,
+        id: this.idValue,
+      },
+    }));
+  }
+
+  //Función usada para las tabs de expediente
+  tabChange(e) {
+    e.preventDefault();
+
+    window.dispatchEvent(new CustomEvent('operationTurboFrameRefresh', {
       detail: {
         url: this.urlValue,
         id: this.idValue,
